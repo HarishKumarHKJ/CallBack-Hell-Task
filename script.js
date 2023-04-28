2889ffee24ce6338a3a368f9b58684b0b564e974
Harish Kumar <127541964+HarishKumarHKJ@users.noreply.github.com>
2023-04-29 01:40:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+setTimeout(()=>{
+    document.querySelector('#four').innerHTML = "09"
+     setTimeout(()=>{
+        document.querySelector('#four').innerHTML = "08"
+         setTimeout(()=>{
+            document.querySelector('#four').innerHTML = "07"
+            setTimeout(()=>{
+                document.querySelector('#four').innerHTML = "06"
+                 setTimeout(()=>{
+                    document.querySelector('#four').innerHTML = "05"
+                     setTimeout(()=>{
+                        document.querySelector('#four').innerHTML = "04"
+                        setTimeout(()=>{
+                            document.querySelector('#four').innerHTML = "03"
+                            setTimeout(()=>{
+                                document.querySelector('#four').innerHTML = "02"
+                                 setTimeout(()=>{
+                                    document.querySelector('#four').innerHTML = "01"
+                                    setTimeout(()=>{
+                                        document.querySelector('#four').innerHTML = "00"
+                                         setTimeout(()=>{
+                                            document.body.style.backgroundColor="#000"
+                                            document.body.innerHTML = `<div class="container">
+                                            <div class="countdown">
+                                            <img style="width:75%" src="independence-day-2018.png" alt="">
+                                            </div>
+                                        </div>`
+                                         })
+                                    },2000)
+                                 },2000)
+                            },2000)
+                        },2000)
+                     },2000)
+                 },2000)
+            },2000)
+         },2000)
+     },2000)
+},2000)
